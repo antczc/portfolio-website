aa9cf41fc85d2e0ff6f8d551c8d2109c661054f2
Anthony Chen <iamantt96@gmail.com>
2023-11-08 02:23:53 +0800

    Upload of website files
    files related to the portfolio website

---
+++ components/about.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function About() {
+  return (
+  <section className="mb-28 max-w-[45rem]
+  text-center leading-8 sm:mb-40">
+    <h2 className="text-3xl font-medium
+    capitalize mb-8">About Me</h2>
+    <p className="mb-3">
+        After graduating with a diploma in{" "}
+        <span className="font-medium">Business Informatics</span>, I decided to pursue my
+        passion for programming. I enrolled in a university and studied{" "}
+        <span className="font-medium">Computer Science</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          Java , C# , C++ , and Python
+        </span>
+        . I am also familiar with Oracle SQL , MongoDB and Web development. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        engineer.
+    </p>
+
+    <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">new web technologies</span>
+    </p>
+
+  </section>
+
+  );
+}
