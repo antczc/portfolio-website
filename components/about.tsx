c8cfe2c23c27363b68c30ca657f1e6f45c5d7b60
Anthony Chen <iamantt96@gmail.com>
2023-11-08 04:28:47 +0800

    Development work

--- components/about.tsx
+++ components/about.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import React from 'react'
+import SectionHeading from './section-heading';
+import { motion } from 'framer-motion';
 
 export default function About() {
   return (
-  <section className="mb-28 max-w-[45rem]
-  text-center leading-8 sm:mb-40">
-    <h2 className="text-3xl font-medium
-    capitalize mb-8">About Me</h2>
+  <motion.section className="mb-28 max-w-[45rem]
+  text-center leading-8 sm:mb-40"
+   initial={{ opacity: 0 , y:100 }}
+   animate={{ opacity: 1 , y:0 }}
+   transition={{ delay: 0.175 }} 
+  >
+    <SectionHeading>About Me</SectionHeading>
     <p className="mb-3">
         After graduating with a diploma in{" "}
         <span className="font-medium">Business Informatics</span>, I decided to pursue my
@@ -25,14 +32,14 @@ export default function About() {
     </p>
 
     <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
+        <span className="italic">When i am not coding</span>, I enjoy playing
         video games, watching movies. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
         <span className="font-medium">new web technologies</span>
     </p>
 
-  </section>
+  </motion.section>
 
   );
 }
