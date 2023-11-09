20fe74bb7505d74a133fb42cb570a51eed0bd8e1
Anthony Chen <iamantt96@gmail.com>
2023-11-10 07:20:47 +0800

    Development

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import Image from 'next/image'
-import React from 'react'
+import React  from 'react'
 import { motion } from 'framer-motion'
 import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithubSquare } from 'react-icons/fa';
+import { useSectionInView } from '@/lib/hooks';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
-export default function intro() {
+export default function Intro() {
+
+  const {ref} = useSectionInView("Home", 0.5);
+  const {
+    setActiveSection,
+    setTimeOfLastClick,
+  } =
+    useActiveSectionContext();
+
+
   return (
-    <section className="mb-28 max-w-[50rem]
-    text-center sm:mb-0">
+    <section ref={ref} id="home" className="mb-28 max-w-[50rem]
+    text-center sm:mb-0 scroll-mt-[100rem]">
       <div className='flex items-center justify-center'>
         <div className="relative">
           <motion.div
@@ -54,7 +65,7 @@ export default function intro() {
       initial={{ opacity: 0, y:100 }}
       animate={{ opacity: 1, y:0 }} 
       >
-        <span className="font-bold">Hello, I'm Anthony.</span> I'm an aspiring {" "}
+        <span className="font-bold">Hello, I&apos;m Anthony.</span> I&apos;m an aspiring {" "}
         <span className="font-bold">Software Engineer</span> with a major in{" "}
         <span className="font-bold">Computer Science</span>. I enjoy
         building <span className="italic">softwares and developing web applications</span>. 
@@ -78,6 +89,10 @@ export default function intro() {
         focus:scale-110 hover:scale-110 
         hover:bg-gray-950 active:scale-105
         transition"
+        onClick={() => {
+          setActiveSection("Contact");
+          setTimeOfLastClick(Date.now());
+        }}
         >
           Contact me here <BsArrowRight
           className="opacity-70 
@@ -88,8 +103,7 @@ export default function intro() {
         rounded-full outline-none
         focus:scale-110 hover:scale-110 
         active:scale-105
-        transition cursor-pointer border 
-        border-black/10" 
+        transition cursor-pointer borderBlack" 
         href="/CV.pdf" download>
           Download CV {" "}
           <HiDownload className="opacity-60 
@@ -97,7 +111,7 @@ export default function intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          className="bg-black p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-black"
           href="https://linkedin.com"
           target="_blank"
         >
@@ -105,7 +119,7 @@ export default function intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          className="bg-black p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-black"
           href="https://github.com"
           target="_blank"
         >
